cfb75802f77af9621a2ec6aa60ad48b439d341da
Diar Ardiansyah <v-diar.ardiansyah@btpn.com>
2023-07-02 21:20:41 +0700

    Update

--- test1.js
+++ test1.js
@@ -1,4 +1,11 @@
-for (let i = 1; i <= 10; i++) {
+// for (let i = 1; i <= 10; i++) {
 
-    console.log('Hello world ' + i)
-}
+//     console.log('Hello world ' + i)
+// }
+
+let inputUser = 10
+let inputUser2 = 5
+
+let jumlah = inputUser * inputUser2
+
+console.log("Jumlah nya adalah " + jumlah)
